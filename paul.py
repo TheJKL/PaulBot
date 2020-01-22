bca980d7e5221ecaa135bc8a4b64b2221ca055f8
TheJKL <vanjackal@protonmail.com>
2020-01-21 20:26:42 -0600

    Made the ';;embed' command for testing embed's

--- paul.py
+++ paul.py
@@ -16,4 +16,13 @@ async def on_ready():
 async def meow(ctx):
     await ctx.send("MEEEEEEOOOOOOWWWW!!!!!!!   *Translation*: **GIB FOOD!**")
 
+@bot.command(name = "embed")#embed message test
+async def embedTest(ctx):
+    embed = discord.Embed(title = "Test", description = "A test embed that will eventually be used for statistics.", color = 0x672aff)
+    embed.add_field(name = "Field 1", value = 1337, inline = False)
+    embed.add_field(name = "Field 2", value = "YOLO!")
+    embed.add_field(name = "Field 3", value = "Swag")
+    embed.set_image(url = "https://cdn.discordapp.com/attachments/522136892448178206/667924013967867934/image0.jpg")
+    await ctx.send(embed = embed)
+
 bot.run(token)
